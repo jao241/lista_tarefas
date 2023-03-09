7c30c212c659347d8b95d484951318f619fdd11f
João Vitor Lima Lago Santos <vorque45@gmail.com>
2023-03-09 17:36:01 -0300

    Adição de formulário Adição de lista Adição de icones Adição de funções para editar e remover

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import './Main.css';
 
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+
 export default class Main extends Component {
     state = {
         novaTarefa: '',
+        tarefas: []
     };
+
+    handleSubmit = (event) => {
+      event.preventDefault();
+
+      const { tarefas } = this.state;
+      let { novaTarefa } = this.state;
+
+      novaTarefa = novaTarefa.trim();
+      if(tarefas.indexOf(novaTarefa) == -1) {
+        const novasTarefas = [...tarefas, novaTarefa];
+
+        this.setState({
+          tarefas: [...novasTarefas],
+          novaTarefa: '',
+        });
+      }
+      else {
+        // Criar ação de texto já existente
+      }
+    }
 
     handleChange = (event)=> {
         this.setState({
@@ -12,17 +40,46 @@ export default class Main extends Component {
         });
     }
 
+    handleEdit = (event, index) => {
+
+    }
+
+    handleDelete = (event, index) => {
+      const { tarefas } = this.state;
+
+      const novasTarefas = [...tarefas];
+
+      novasTarefas.splice(index, 1);
+
+      this.setState({
+        tarefas: [...novasTarefas],
+      });
+    }
+
     render() {
-        const { novaTarefa } = this.state;
+        const { novaTarefa, tarefas } = this.state;
 
         return (
             <div className='main'>
                 <h1>Lista de tarefas</h1>
-                <h1>{novaTarefa}</h1>
-                <form action='#'>
-                    <input onChange={this.handleChange} type='text' />
-                    <button type='submit'>Enviar</button>
+                <form onSubmit={this.handleSubmit} action='#' className='form'>
+                    <input onChange={this.handleChange} type='text' value={novaTarefa} />
+                    <button type='submit' title='Adicionar nova tarefa'>
+                      <FaPlus />
+                    </button>
                 </form>
+
+                <ul className='tarefas'>
+                    {tarefas.map((value, index) => (
+                      <li key={value}>
+                        {value}
+                        <div>
+                          <FaEdit onClick={(event) => this.handleEdit(event, index)} className='edit' />
+                          <FaWindowClose onClick={(event) => this.handleDelete(event, index)} className='delete' />
+                        </div>
+                      </li>
+                    ))}
+                </ul>
             </div>
         );
     }
